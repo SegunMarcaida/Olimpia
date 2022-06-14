e881950507bab635915b5569f2627ed05a02d1e2
segun <segundo.marcaida@ing.austral.edu.ar>
2022-06-14 15:42:39 -0300

    cmo le digo q me tiene loko

--- controllers/shared/profile.js
+++ controllers/shared/profile.js
@@ -1,10 +1,12 @@
 const {Field} = require("../../models/field");
 const {ObjectId} = require("mongodb");
+const {User} = require("../../models/User");
 module.exports= function(){
 return  async function (req, res) {
-    const id = new ObjectId(req.body.courtId)
+    let id = req.user._id
+     id = new ObjectId(id)
 
-    await Field.findOne({_id: id}).then(field => {
+    await User.findOne({_id: id}).then(field => {
         if (field) {
             res.send(200, field)
         } else {
